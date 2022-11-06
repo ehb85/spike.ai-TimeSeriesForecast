16f5145c37abd5b2dfbb54b92d4eef0fb782ab9f
ehb85 <Eugenio.huerta@me.com>
2022-11-05 21:17:12 -0300

    2nd commit

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/train/', methods=['GET', 'POST'])
+def add_message():
+    content = request.json
+    return content
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
