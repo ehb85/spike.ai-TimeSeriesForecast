d61db6dc982959710716dbf9c7ee43c0482192e5
ehb85 <41530589+ehb85@users.noreply.github.com>
2022-11-06 02:56:23 -0300

    m

--- app.py
+++ app.py
@@ -48,6 +48,16 @@ def train():
         # create model instance
         bst = xgb.XGBClassifier(n_estimators=n_estimators, max_depth=max_depth, learning_rate=learning_rate, min_child_weight=min_child_weight, booster=booster)
 
+        orderData_Features = pd.read_pickle("../orderData_Features_wLags.pkl") 
+        TARGET = 'qty'
+        FEATURES = ['outlier', 'cluster', 'date_day_of_week', 'date_day_of_month', 'date_day_of_year', 'date_week', 'date_month', 'qty_1DA', 'qty_2DA', 'qty_3DA', 'qty_4DA', 'qty_5DA', 'qty_6DA', 'qty_7DA', 'qty_8DA', 'qty_9DA', 'qty_10DA', 'qty_11DA', 'qty_12DA', 'qty_13DA', 'qty_14DA', 'qty_21DA', 'qty_28DA']
+
+        X_train = orderData_Features[orderData_Features.fecha < '2022-04-01'][FEATURES]
+        y_train = orderData_Features[orderData_Features.fecha < '2022-04-01'][TARGET]
+        
+        X_test = orderData_Features[orderData_Features.fecha > '2022-04-03'][FEATURES]
+        y_test = orderData_Features[orderData_Features.fecha > '2022-04-03'][TARGET]
+
 
         
 
