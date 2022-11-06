b2dd41ed2638341b04ea24c5b4730e66c4bbf3ec
ehb85 <41530589+ehb85@users.noreply.github.com>
2022-11-05 21:41:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,13 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return 'Index Page'
 
 @app.route('/train')
-def add_message():
-    if request.method == 'POST':
-        content = request.json
-        aux = content
-    else:
-        aux = "test..."
-    return aux
-
-
+def train():
+    return 'Hello, World'
 
 
 if __name__ == "__main__":
