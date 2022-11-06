7d66511dbac574d9fc8202bd06216a99582b5fcb
ehb85 <41530589+ehb85@users.noreply.github.com>
2022-11-06 03:33:10 -0300

    s

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def upload_file():
 @app.route('/load_data', methods=['POST'])
 def load_data():
     # preprocesa tdos
-    orderData_Features = pd.read_pickle("/home/ec2-user/orderData_Features_wLags.pkl") 
+    orderData_Features = pd.read_pickle(r"../../ec2-user/orderData_Features_wLags.pkl") 
     TARGET = 'qty'
     FEATURES = ['outlier', 'cluster', 'date_day_of_week', 'date_day_of_month', 'date_day_of_year', 'date_week', 'date_month', 'qty_1DA', 'qty_2DA', 'qty_3DA', 'qty_4DA', 'qty_5DA', 'qty_6DA', 'qty_7DA', 'qty_8DA', 'qty_9DA', 'qty_10DA', 'qty_11DA', 'qty_12DA', 'qty_13DA', 'qty_14DA', 'qty_21DA', 'qty_28DA']
 
@@ -48,7 +48,7 @@ def train():
         # create model instance
         bst = xgb.XGBClassifier(n_estimators=n_estimators, max_depth=max_depth, learning_rate=learning_rate, min_child_weight=min_child_weight, booster=booster)
 
-        orderData_Features = pd.read_pickle("/home/ec2-user/orderData_Features_wLags.pkl") 
+        orderData_Features = pd.read_pickle(r"/home/ec2-user/orderData_Features_wLags.pkl") 
         TARGET = 'qty'
         FEATURES = ['outlier', 'cluster', 'date_day_of_week', 'date_day_of_month', 'date_day_of_year', 'date_week', 'date_month', 'qty_1DA', 'qty_2DA', 'qty_3DA', 'qty_4DA', 'qty_5DA', 'qty_6DA', 'qty_7DA', 'qty_8DA', 'qty_9DA', 'qty_10DA', 'qty_11DA', 'qty_12DA', 'qty_13DA', 'qty_14DA', 'qty_21DA', 'qty_28DA']
 
