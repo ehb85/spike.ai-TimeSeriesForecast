775edcdd7564669f6e02570c104ac34345b788f8
ehb85 <41530589+ehb85@users.noreply.github.com>
2022-11-08 21:28:26 -0300

    m

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def train():
 
         return {'hyperparameters': {'n_estimators': n_estimators,'max_depth':max_depth,'learning_rate':learning_rate, 'min_child_weight': min_child_weight,'booster':booster },
                 'results': {'train_data_result': bst.evals_result()['validation_0']['rmse'][-1], 'test_data_result':  bst.evals_result()['validation_1']['rmse'][-1]}, 
-                'execution_time': {'time_model_execution': (timestamp_end-timestamp_end_extraction).total_seconds(),'time_data_extraction': (timestamp_end_extraction-timestamp_start).total_seconds()}
+                'execution_time': {'time_model_execution': (timestamp_end-timestamp_end_extraction).total_seconds(),'time_data_extraction': (timestamp_end_extraction-timestamp_start).total_seconds()},
                 'model': {'save_nodel_file': model_name}
                 }
     else:
