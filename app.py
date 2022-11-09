41eafe20892b8d615cddf81182def20df0cdc6a9
ehb85 <41530589+ehb85@users.noreply.github.com>
2022-11-08 23:27:07 -0300

    m

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def make_prediction():
     X_predict = np.array(content['X'])
     y_predict = bst.predict(X_predict)
 
-    return y_predict
+    return json.dumps(y_predict, cls=NumpyArrayEncoder)  
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
