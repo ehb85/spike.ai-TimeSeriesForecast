4b4d869b789818590628b98907fc88c5805dd257
ehb85 <41530589+ehb85@users.noreply.github.com>
2022-11-10 18:37:10 -0300

    a

--- app.py
+++ app.py
@@ -38,15 +38,17 @@ def load_data():
         s3 = boto3.client('s3', aws_access_key_id=source['aws_access_key_id'], aws_secret_access_key= source['aws_secret_access_key'])
 
         bucket_name = source['bucket_name']
+        orderData_Features = pd.DataFrame()
 
         for s3_object in source['object']:
 
             obj = s3.get_object(Bucket=bucket_name, Key=s3_object)
-            orderData_Features = pd.read_json(io.BytesIO(obj['Body'].read()))
-            orderData_Features['fecha'] =  pd.to_datetime(orderData_Features['fecha'],unit='ms')
+            orderData_Features_aux = pd.read_json(io.BytesIO(obj['Body'].read()))
+            orderData_Features_aux['fecha'] =  pd.to_datetime(orderData_Features_aux['fecha'],unit='ms')
+            orderData_Features = orderData_Features.append(orderData_Features_aux)
             
-            file_name = s3_object[0:s3_object.find('.')] + '_' + str(round(time.time())) + '.pkl'
-            orderData_Features.to_pickle(file_name)
+        file_name = s3_object[0:s3_object.find('.')] + '_' + str(round(time.time())) + '.pkl'
+        orderData_Features.to_pickle(file_name)
         
         return {'file_name': file_name, 'registros': len(orderData_Features)}
 
